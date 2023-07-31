de6d8ff750046eeca6ea2ddc8d6af2efbf778200
om raj indra <omrajindra.ori@gmail.com>
2023-07-31 08:33:56 +0545

    dotenvsetup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "" });
 
 const app = express();
 
@@ -7,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`.bgCyan.white);
