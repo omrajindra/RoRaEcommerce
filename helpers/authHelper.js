89175cc70929dbfe8c1e046923fa9eba85a41f1a
om raj indra <omrajindra.ori@gmail.com>
2023-08-02 17:00:06 +0545

    complete register but have problem not able to find

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -3,12 +3,12 @@ import bcrypt from "bcrypt";
 export const hashPassword = async (password) => {
   try {
     const saltRounds = 10;
-    const hashPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
   } catch (error) {
     console.log(error);
   }
 };
 export const comparePassword = async (password, hashedPassword) => {
-  return bcrypt.compare(password, hashPassword);
+  return bcrypt.compare(password, hashedPassword);
 };
